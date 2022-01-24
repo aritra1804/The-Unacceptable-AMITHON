50b175a32bfa1bd1da67652c3ec999c6789adfc4
Abhinav Maharana <abhinavmaharana.004@gmail.com>
2022-01-24 23:59:12 +0530

    🐛 Footer

--- frontend/src/components/HomePage/Footer/Footer.js
+++ frontend/src/components/HomePage/Footer/Footer.js
@@ -5,16 +5,24 @@ function Footer() {
     <footer>
       <div className="grid grid-rows-1 md:grid-cols-5 px-[100px] py-[80px] gap-x-[150px] mt-[80px] bg-[#6F5DE0] text-white">
         <div className="space-y-4 text-3xl text-white w-[380px]">
-          <p>We love 3D models and the people who build them.</p>
+          <p>We love Security and the products that help the world.</p>
         </div>
         <div className="space-y-4 text-sm text-white"></div>
         <div className="space-y-4 text-lg text-white">
-          <h5 className="font-bold">ABOUT</h5>
+          {/* <h5 className="font-bold">ABOUT</h5>
           <p>How Airbnb works</p>
           <p>Newsroom</p>
           <p>Investors</p>
           <p>Airbnb Plus</p>
-          <p>Airbnb Luxe</p>
+          <p>Airbnb Luxe</p> */}
+        </div>
+        <div className="space-y-4 text-lg text-white">
+          {/* <h5 className="font-bold">ABOUT</h5>
+          <p>How Airbnb works</p>
+          <p>Newsroom</p>
+          <p>Investors</p>
+          <p>Airbnb Plus</p>
+          <p>Airbnb Luxe</p> */}
         </div>
         <div className="space-y-4 text-lg text-white">
           <h5 className="font-bold">ABOUT</h5>
@@ -24,14 +32,8 @@ function Footer() {
           <p>Airbnb Plus</p>
           <p>Airbnb Luxe</p>
         </div>
-        <div className="space-y-4 text-lg text-white">
-          <h5 className="font-bold">ABOUT</h5>
-          <p>How Airbnb works</p>
-          <p>Newsroom</p>
-          <p>Investors</p>
-          <p>Airbnb Plus</p>
-          <p>Airbnb Luxe</p>
-        </div>
+        <p className='w-[380px] mt-[50px]'>© Copyright Starx Company. All Rights Reserved
+Made in Amithon by The Unacceptables</p>
       </div>
     </footer>
   );
--- frontend/src/components/HomePage/Team/Team.js
+++ frontend/src/components/HomePage/Team/Team.js
@@ -13,8 +13,7 @@ function Team() {
         <div className="grid grid-row-2 lg:grid-rows-2 space-y-2 ">
           <h1 className="font-bold text-[#6F5DE0] text-6xl">Our Team</h1>
           <p className="xl:w-[700px] 2xl:w-[700px]  text-2xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit.
+          Meet our exceptionally talented team that made the Starx Company a huge success.
           </p>
           <div className=" mb-5 w-[1200px] ml-[-530px] 2xl:ml-[-500px]">
           <TeamMembers />
