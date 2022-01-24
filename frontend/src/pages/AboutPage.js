4d80b5c725e9f6e0654c2a00577b2cc53206cb75
Abhinav Maharana <abhinavmaharana.004@gmail.com>
2022-01-24 19:34:27 +0530

    ✅ Added Motivation Section

--- frontend/src/pages/AboutPage.js
+++ frontend/src/pages/AboutPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AboutSection from '../components/AboutPage/AboutSection/AboutSection';
+import Motivation from '../components/AboutPage/Motivation/Motivation';
 import Footer from '../components/HomePage/Footer/Footer';
 import Header from '../components/HomePage/Header/Header';
 
@@ -8,6 +9,7 @@ const AboutPage = () => {
       <>
       <Header />
         <AboutSection />
+        <Motivation />
         <Footer />
       </>
   );
