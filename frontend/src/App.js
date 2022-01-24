e40eb71028120bc4bbc8de14e71c16c2f3f69e06
Abhinav Maharana <abhinavmaharana.004@gmail.com>
2022-01-25 00:08:13 +0530

    ✨ Restricted Mobile view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css';
+import {isMobile} from 'react-device-detect';
 import AboutPage from './pages/AboutPage';
 import Home from './pages/Home';
 
 function App() {
+  if (isMobile) {
+    return <div style={{display:"flex", flexDirection:"column", alignItems:"center",textAlign:"center", marginTop:"20%", marginLeft:"3%"}}> This website is not available for mobile and ipad. <br/> It will be available in the form of app. <br/> Kindly check it in desktop. Soon App will be released <lottie-player src="https://assets4.lottiefiles.com/packages/lf20_w51pcehl.json"  background="transparent"  speed="1"  style={{width:"500px", marginTop:"5%"}}  loop  autoplay></lottie-player></div>
+  }
   return (
     <BrowserRouter>
       <Routes>
