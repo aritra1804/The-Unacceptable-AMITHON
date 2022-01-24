f8979110eb6e77597d6d5c368980000ec9935abd
Abhinav Maharana <abhinavmaharana.004@gmail.com>
2022-01-24 22:56:06 +0530

    🐛 Fixes

--- frontend/src/components/HomePage/Team/TeamMembers/TeamMembers.js
+++ frontend/src/components/HomePage/Team/TeamMembers/TeamMembers.js
@@ -8,10 +8,10 @@ function TeamMembers() {
   return (
       <div className='mt-[40px]'>
           <div className='flex space-x-3 '>
-            <div className="bg-[#F6F6F6] shadow-xl xl:w-[330px] xl:h-[390px] xl:mt-[50px] 2xl:mt-[50px]  2xl:w-[350px] 2xl:h-[350px] rounded-lg" > <img src={Abhinav} alt="" className='w-[500px] h-[350px] rounded-lg object-cover'/></div>
-            <div className="bg-[#F6F6F6] shadow-xl xl:w-[330px] xl:h-[390px] xl:mt-[50px] 2xl:mt-[50px]  2xl:w-[350px] 2xl:h-[350px] rounded-lg"><img src={Aritra} alt="" className='w-[500px] h-[350px] rounded-lg object-cover'/></div>
-            <div className="bg-[#F6F6F6] shadow-xl xl:w-[330px] xl:h-[390px] xl:mt-[50px] 2xl:mt-[50px]  2xl:w-[350px] 2xl:h-[350px] rounded-lg"><img src={Om} alt="" className='w-[500px] h-[350px] rounded-lg object-cover'/></div>
-            <div className="bg-[#F6F6F6] shadow-xl xl:w-[330px] xl:h-[390px] xl:mt-[50px] 2xl:mt-[50px]  2xl:w-[350px] 2xl:h-[350px] rounded-lg"><img src={Farhan} alt="" className='w-[500px] h-[350px] rounded-lg object-cover'/></div>
+            <div className="bg-[#F6F6F6] shadow-xl xl:w-[330px] xl:h-[350px] xl:mt-[50px] 2xl:mt-[50px]  2xl:w-[350px] 2xl:h-[350px] rounded-lg" > <img src={Abhinav} alt="" className='w-[500px] h-[350px] rounded-lg object-cover'/></div>
+            <div className="bg-[#F6F6F6] shadow-xl xl:w-[330px] xl:h-[350px] xl:mt-[50px] 2xl:mt-[50px]  2xl:w-[350px] 2xl:h-[350px] rounded-lg"><img src={Aritra} alt="" className='w-[500px] h-[350px] rounded-lg object-cover'/></div>
+            <div className="bg-[#F6F6F6] shadow-xl xl:w-[330px] xl:h-[350px] xl:mt-[50px] 2xl:mt-[50px]  2xl:w-[350px] 2xl:h-[350px] rounded-lg"><img src={Om} alt="" className='w-[500px] h-[350px] rounded-lg object-cover'/></div>
+            <div className="bg-[#F6F6F6] shadow-xl xl:w-[330px] xl:h-[350px] xl:mt-[50px] 2xl:mt-[50px]  2xl:w-[350px] 2xl:h-[350px] rounded-lg"><img src={Farhan} alt="" className='w-[500px] h-[350px] rounded-lg object-cover'/></div>
           </div>
       </div>
   );
